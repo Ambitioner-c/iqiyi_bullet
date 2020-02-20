94d42a1c33a00d3ebeee90dd9c872d1a89f3d7c3
Ambitioner-c <656359504@qq.com>
2020-02-21 05:32:04 +0800

    获取视频的弹幕

--- python/bullet.py
+++ python/bullet.py
@@ -1,16 +1,18 @@
 import zlib
 import requests
 
+tv_id_module = __import__('tv_id')
 
-def bullet(tv_id):
 
-    for page in range(12, 17):
+def get_bullet(tv_id):
+    for page in range(1, 17):
         # https://cmts.iqiyi.com/bullet/tv_id[-4:-2]/tv_id[-2:]/tv_id_300_x.z
         url = 'https://cmts.iqiyi.com/bullet/'\
               + tv_id[-4:-2] + '/'\
               + tv_id[-2:] + '/'\
               + tv_id + '_300_'\
               + str(page) + '.z'
+        print(url)
 
         # 请求弹幕压缩文件
         res = requests.get(url).content
@@ -22,10 +24,14 @@ def bullet(tv_id):
             path = '../data/' + tv_id + '_300_' + str(page) + '.xml'
             with open(path, 'w', encoding='utf-8') as f:
                 f.write(xml)
-        finally:
+        except:
             return
 
 
 if __name__ == '__main__':
-    my_tv_id = '11298454000'
-    bullet(my_tv_id)
+    # aid
+    my_aid = '212447801'
+    # tv_id列表
+    my_tv_id_list = tv_id_module.get_tv_id(my_aid)
+    for i in my_tv_id_list:
+        get_bullet(str(i))
